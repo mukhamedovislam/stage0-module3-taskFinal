97baae21582727494f1b3c83e9bbac7f3d762b0c
mukhamedovislam <bozuy24@gmail.com>
2022-09-09 11:57:39 +0400

    First Solution

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,14 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+    	int sumOfDigits = 0;
+	while (number != 0) {
+		sumOfDigits = sumOfDigits + number % 10;
+		number = number / 10;
+	}
+	System.out.println(sumOfDigits);
+
+
+
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,5 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
+    	double temperatureF = temperatureCelcius * 1.8 + 32;
+	System.out.println(temperatureF);
     }
 }
