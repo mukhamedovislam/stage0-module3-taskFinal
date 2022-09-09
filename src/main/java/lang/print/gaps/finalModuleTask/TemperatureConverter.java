2effe45020657999797174838b842c8799cd8fdc
mukhamedovislam <bozuy24@gmail.com>
2022-09-09 12:00:30 +0400

    Second Solution

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,7 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
-    	double temperatureF = temperatureCelcius * 1.8 + 32;
+    	double temperatureF = temperatureCelsius * 1.8 + 32;
 	System.out.println(temperatureF);
     }
 }
